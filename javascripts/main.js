1767fa746b6aec309a13728b2b7599d34abfc2aa
everm1nd <everm1nd.mail@gmail.com>
2015-02-22 14:27:47 +0700

    refactored JS. moved video functionality into VideoController

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,63 +1,12 @@
-var videoCtrl;
-var RANDOM_MATRIX = [
-  { seconds: 14 },
-  { seconds: 30 },
-  { seconds: 42 },
-  { seconds: 71 },
-  { seconds: 94 },
-  { seconds: 103 },
-  { seconds: 131 },
-  { seconds: 152 },
-  { seconds: 170 },
-  { SMPTE: '00:01:12:22' }
-]
+var videoController;
 
-function irand(min, max) {
-  return Math.round(rand(min, max));
-}
-
-function rand(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function video() {
-  return videoCtrl.video;
-}
-
-function play() {
-	video().play()
-}
-
-function pause() {
-	video().pause();
-}
-
-function matrix_random() {
-	return RANDOM_MATRIX[irand(0,RANDOM_MATRIX.length - 1)]
-}
-
-function random() {
-	return rand(0,180)
-}
-
-function call_jesus() {
-	videoCtrl.seekTo(matrix_random());
-}
-
-function init() {
-  videoCtrl = VideoFrame({
-    id : 'videoPlayer',
-    frameRate: FrameRates.film,
-    callback : function(response) {
-        console.log('callback response: ' + response);
-    }
-  });
+function bind_events() {
+  $('#videoPlayer').click(videoController.jump);
+	$(document).keypress(videoController.jump);
 }
 
 $(document).ready(function() {
-  init();
-	play();
-	$(document).keypress(function() {
-		call_jesus();
-	});
+  videoController = new VideoController();
+	videoController.play();
+  bind_events();
 });
---
+++ javascripts/videoController.js
@@ -0,0 +1,44 @@
+function VideoController() {
+  var controller,
+      video;
+
+  var RANDOM_MATRIX = [
+    { seconds: 14 },
+    { seconds: 30 },
+    { seconds: 42 },
+    { seconds: 71 },
+    { seconds: 94 },
+    { seconds: 103 },
+    { seconds: 131 },
+    { seconds: 152 },
+    { seconds: 170 },
+    { SMPTE: '00:01:12:22' }
+  ]
+
+  // public
+
+  this.play = function(){
+  	video.play()
+  }
+
+  this.pause = function() {
+  	video.pause();
+  }
+
+  this.jump = function() {
+    controller.seekTo(_.sample(RANDOM_MATRIX));
+  };
+
+  // private
+
+  (function init() {
+    controller = VideoFrame({
+      id : 'videoPlayer',
+      frameRate: FrameRates.film,
+      callback : function(response) {
+          console.log('callback response: ' + response);
+      }
+    });
+    video = controller.video;
+  })();
+};
